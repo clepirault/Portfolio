79162f13cfcb74cf1fd790b1a08011c8047b200c
clepirault <clepirault@gmail.com>
2021-10-26 19:52:58 +0200

    env variable vercel

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -6,12 +6,21 @@ const sendEmail = async (req, res) => {
   try {
     const body = JSON.parse(JSON.stringify(req.body)) || {};
     console.log(body);
-    await sendgrid.send({
+
+    const message = {
       to: 'clepidev@gmail.com',
       from: 'clepidev@gmail.com',
       subject: `${body.Subject}`,
-      html: `<body><h3>You've got a new mail from ${body.FirstName} ${body.LastName}, the email is: ✉️${body.EmailAddress}</h3><p>Message:</p><p>${body.Message}</p></body>`,
+      html: `<body><h3>You've got a new mail from ${body.FirstName} ${body.LastName}, the email is: ✉️${body.EmailAddress}</h3><p>Message:</p><p>${body.Message}</p></body>` 
+    }
+
+    await sendgrid.send(message, (error) => {
+      if (error) {
+        console.log('Something went wrong');
+      } else {
+        console.log('Email has been sent')}
     });
+
   } catch (error) {
     return res.status(error.statusCode || 500).json({error: error.message});
   }
