c6ca9911b95405252e5b3c205e0ff0482ea0f2cb
clepirault <clepirault@gmail.com>
2021-10-28 10:35:45 +0200

    add button on projectHero to click the whole div

--- components/project/ProjectHero.tsx
+++ components/project/ProjectHero.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type Props = {
   title: string;
@@ -10,12 +10,19 @@ type Props = {
 
 const ProjectHero: FC<Props> = (props) => {
   const { title, uid } = props;
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/portfolio/${uid}`);
+  };
+
   return (
-    <div className='flex justify-center items-center w-80 h-80 m-4 bg-black text-white sm:border-solid sm:border-[3px] sm:border-black sm:bg-gray-100 sm:text-black sm:hover:bg-black sm:hover:text-white'>
-      <Link href={`/portfolio/${uid}`}>
-        <a className='capitalize tracking-widest'>{title}</a>
-      </Link>
-    </div>
+    <button onClick={handleClick}>
+      <div className='flex justify-center items-center w-80 h-80 m-4 bg-black text-white sm:border-solid sm:border-[3px] sm:border-black sm:bg-gray-100 sm:text-black sm:hover:bg-black sm:hover:text-white'>
+        <h3 className='capitalize tracking-widest'>{title}</h3>
+      </div>
+    </button>
   );
 };
 
